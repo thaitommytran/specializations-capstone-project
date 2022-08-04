e2e6cefe728d804a15dece711eb7d643e3904d63
Thai Tran <thai.tommy.tran@gmail.com>
2022-08-04 10:24:03 -0500

    added endpoints and connected database

--- client/src/components/StoredAccounts.jsx
+++ client/src/components/StoredAccounts.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/StoredAccounts.css";
+import axios from "axios";
 
 const StoredAccounts = () => {
+  const [accounts, setAccounts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3045/accounts")
+      .then((res) => setAccounts(res.data));
+  }, []);
+
+  console.log(accounts);
+
   return (
     <div>
       StoredAccounts
       <h1>Your Accounts</h1>
       <hr />
-      <div className="stored-accounts">
-        
-      </div>
+      <div className="stored-accounts"></div>
     </div>
   );
 };
