9086df1c41976aff987ae185fe3a064576a897b4
Thai Tran <thai.tommy.tran@gmail.com>
2022-08-15 09:48:24 -0500

    matched initial styling from add accounts to stored accounts

--- client/src/components/AccountCard.jsx
+++ client/src/components/AccountCard.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const AccountCard = (props) => {
   return (
     <div>
-        <h3>{props.account.website}</h3>
-        <h3>{props.account.username}</h3>
-        <h3>{props.account.password}</h3>
+      <p className="initial"></p>
+      <div className="list-content">
+        <p>{props.account.website}</p>
+        <p>{props.account.username}</p>
+        <p>{props.account.password}</p>
+      </div>
+      <button className="del-btn">X</button>
     </div>
-  )
-}
+  );
+};
 
-export default AccountCard
+export default AccountCard;
