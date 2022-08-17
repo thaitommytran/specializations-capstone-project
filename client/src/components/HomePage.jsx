ee4ffa8747cafaecd0693aa972d952348b25aeff
Thai Tran <thai.tommy.tran@gmail.com>
2022-08-17 14:14:36 -0500

    fixed styling layout

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import Header from "./Header";
 import ContainerCard from "./ContainerCard";
 
+import "./styles/HomePage.css";
+
 const HomePage = (props) => {
   return (
-    <div>
-      <Header
-        onLogout={props.onLogout}
-        userId={props.userId}
-      />
+    <div className="home">
+      <Header onLogout={props.onLogout} userId={props.userId} />
       <ContainerCard userId={props.userId} />
     </div>
   );
